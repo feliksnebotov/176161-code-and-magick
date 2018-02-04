ce68c53ac75c8f05f949f2af7daa7d4e876ba4d1
Feliks Nebotov <feliksnotbotov@gmail.com>
2018-02-04 16:10:06 +0300

    Доделал задание и причесал файл по кодгайду

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Облако
+// Облако
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -11,7 +11,7 @@ var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CLOUD_COLOR = '#fff';
 var COLOR_BLACK = '#000';
 
-//Текст
+// Текст
 var TEXT_FONT = '16px PT Mono';
 var TEXT_VICTORY = 'Ура вы победили!';
 var TEXT_VICTORY_COORD_X = 120;
@@ -20,76 +20,61 @@ var TEXT_RESULT = 'Список результатов:';
 var TEXT_RESULT_COORD_X = 120;
 var TEXT_RESULT_COORD_Y = 60;
 
-//Диаграмма
+// Диаграмма
 var HISTOGRAM_WIDTH = 40;
 var HISTOGRAM_HEIGHT = 150;
 var HISTOGRAM_COLUMN_GAP = 50;
 var HISTOGRAM_START_X = 155;
-var HISTOGRAM_START_Y = 85; //(270-150)/2=60 высота пространства над и под гистограммой; 280-150+60-25(текст и отступ)
-var HISTOGRAM_TEXT_Y = 255; //280-15(высота текста)-10(отступ)
+var HISTOGRAM_START_Y = 235; // (270-150)/2=60 высота пространства над и под гистограммой; 280-150+60-25(текст и отступ)
+var HISTOGRAM_TEXT_Y = 255; // 280-15(высота текста)-10(отступ)
+var HISTOGRAM_TEXT_GAP = 30;
 var columnColor;
 
-var renderCloud = function(ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
-    var maxElement = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > maxElement) {
-            maxElement = arr[i];
-        }
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
     }
-    return maxElement;
+  }
+  return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
-    //Рисуем облако
-    renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, SHADOW_COLOR);
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+window.renderStatistics = function (ctx, names, times) {
+  // Рисуем облако
+  renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-    ctx.fillStyle = COLOR_BLACK;
-    ctx.font = TEXT_FONT;
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.font = TEXT_FONT;
 
-    ctx.fillText(TEXT_VICTORY, TEXT_VICTORY_COORD_X, TEXT_VICTORY_COORD_Y);
-    ctx.fillText(TEXT_RESULT, TEXT_RESULT_COORD_X, TEXT_RESULT_COORD_Y);
-
-    //Получаем максимальное время прохождения
-    var maxTime = getMaxElement(times);
-
-    //Рисуем гистограмму
-    for (var i = 0; i < names.length; i++) {
-      var time = Math.floor(times[i]);
-        // Рисуем колонку нужного цвета
-        if (names[i] === 'Вы') {
-            columnColor = 'rgba(255, 0, 0, 1)';
-        } else {
-            columnColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
-        }
-
-        ctx.fillStyle = columnColor;
-        ctx.fillRect(HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, (HISTOGRAM_HEIGHT * times[i]) / maxTime);
-
-        ctx.fillStyle = COLOR_BLACK;
-        ctx.fillText(time, HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_TEXT_Y);
-        ctx.fillText(names[i], HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_TEXT_Y);
-
-    }
-
-};
-
-/*  ctx.fillText(TEXT_VICTORY, TEXT_VICTORY_COORD_X, TEXT_VICTORY_COORD_Y);
+  ctx.fillText(TEXT_VICTORY, TEXT_VICTORY_COORD_X, TEXT_VICTORY_COORD_Y);
   ctx.fillText(TEXT_RESULT, TEXT_RESULT_COORD_X, TEXT_RESULT_COORD_Y);
 
-  ctx.fillText('Вы', HISTOGRAM_START_X, HISTOGRAM_TEXT_Y);
-  ctx.fillRect(HISTOGRAM_START_X, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT);
+  // Получаем максимальное время прохождения
+  var maxTime = getMaxElement(times);
 
-  ctx.fillText('Иван', HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 1, HISTOGRAM_TEXT_Y);
-  ctx.fillRect(HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 1, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT);
+  // Рисуем гистограмму
+  for (var i = 0; i < names.length; i++) {
+    var time = Math.floor(times[i]);
+    // Рисуем колонку нужного цвета
+    if (names[i] === 'Вы') {
+      columnColor = 'rgba(255, 0, 0, 1)';
+    } else {
+      columnColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
 
-  ctx.fillText('Юлия', HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 2, HISTOGRAM_TEXT_Y);
-  ctx.fillRect(HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 2, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT);
+    ctx.fillStyle = columnColor;
+    ctx.fillRect(HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, (HISTOGRAM_HEIGHT * times[i]) / maxTime * -1);
 
-  ctx.fillText('Алексей', HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 3, HISTOGRAM_TEXT_Y);
-  ctx.fillRect(HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * 3, HISTOGRAM_START_Y, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT);*/
+    ctx.fillStyle = COLOR_BLACK;
+    ctx.fillText(time, HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_TEXT_Y - (HISTOGRAM_HEIGHT * times[i]) / maxTime - HISTOGRAM_TEXT_GAP);
+    ctx.fillText(names[i], HISTOGRAM_START_X + (HISTOGRAM_COLUMN_GAP + HISTOGRAM_WIDTH) * i, HISTOGRAM_TEXT_Y);
+
+  }
+};
